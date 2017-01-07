733c23ec0254a350645241fa8a759f32a987df4e
Alexander Waldemar Ahjolinna <ahjolinna@yahoo.com>
2017-01-07 21:56:07 +0200

    latest changes

--- mpv/etc/scripts/auto-options.lua
+++ mpv/etc/scripts/auto-options.lua
@@ -17,15 +17,12 @@
 
 
 -- Don't do anything when mpv was called with an explicitly passed --vo option
-
 if mp.get_property_bool("option-info/vo/set-from-commandline") then
     return
 end
+
 local f = require 'auto-options-functions'
 local opts = require 'mp.options'
-local utils = require 'mp.utils'
-
--- Specify a VO for each level
 
 local o = {
     hq = "high-quality",
@@ -43,11 +40,20 @@ local o = {
 opts.read_options(o)
 
 
--- Specify mpv options for each level
+-- Specify a VO for each level
 
-local options = {
+vo = {
+    [o.hq] = "opengl-hq",
+    [o.cuda] =  "opengl-hq",
+    [o.svp] =  "opengl-hq",
+    [o.mq] = "opengl-hq",
+    [o.lq] = "opengl",
+}
+
+
+-- Specify VO sub-options for different levels
+vo_opts = {
     [o.hq] = {
-        ["vo"] = "opengl",
         ["scale"]  = "ewa_lanczossharp",
         ["cscale"] = "ewa_lanczossoft",
         ["dscale"] = "mitchell",
@@ -55,63 +61,65 @@ local options = {
         ["scale-antiring"] = "0.8",
         ["cscale-antiring"] = "0.9",
         ["scale-radius"]    = "3",
-        ["interpolation"]     =  "yes",
-        ["interpolation-threshold"] = "0.0001",        
+
+        ["interpolation"]     =  function () return is_high_res(o) and "no" or "yes" end,
+        ["interpolation-threshold"] = "0.0001",
+        
         ["dither-depth"]        = "auto",
         ["scaler-resizes-only"] = "yes",
         ["sigmoid-upscaling"]   = "yes",
         ["correct-downscaling"] = "yes",
-        ["opengl-waitvsync"]           = "yes",
-        ["target-prim"]         = "bt.709",
+        ["waitvsync"]           = "yes",
+        
+        ["gamma"]               = "0.9338",
+        ["target-prim"]         = "bt.2020",
+        ["target-trc"]          = "bt.1886",
         ["3dlut-size"]          = "256x256x256",
         ["blend-subtitles"]     = "video",
         ["icc-contrast"]            = "2000",
-        ["icc-profile"]               = "/usr/share/color/icc/BT.709_Profiles/BT.709.icc",        
-        ["hwdec"] = "auto",
-        ["video-sync"] = "display-resample",
-        ["vd-lavc-threads"] = "16",
-        --["vf-add"] = "vdpaupp=sharpen=0.10:denoise=0.10:deint=yes:deint-mode=temporal-spatial:pullup:hqscaling=1",
+        ["icc-profile"]               = "/usr/share/color/icc/BT.709_Profiles/BT.709.icc",
     },
 
     [o.cuda] = {  
-        ["vo"] = "opengl",
         ["scale"]  = "ewa_lanczossharp",
         ["cscale"] = "ewa_lanczossoft",
         ["dscale"] = "mitchell",
         ["tscale"] = "triangle",
         ["scale-antiring"]  = "0.8",
-        ["cscale-antiring"] = "0.9",        
-        ["interpolation"]     = "yes",
-        ["interpolation-threshold"] = "0.0001",        
+        ["cscale-antiring"] = "0.9",
+        
+        ["interpolation"]     =  function () return is_high_res(o) and "no" or "yes" end,
+        ["interpolation-threshold"] = "0.0001",
+        
         ["dither-depth"]        = "auto",
         ["target-prim"]         = "bt.709",
         ["correct-downscaling"] = "yes",
-        ["opengl-waitvsync"]           = "yes",
+        ["waitvsync"]           = "yes",
+        ["vd-lavc-o=deint"] = "adaptive",
+        
+        ["gamma"]               = "0.9338",
+        ["target-prim"]         = "bt.2020",
+        ["target-trc"]          = "bt.1886",
         ["3dlut-size"]          = "256x256x256",
-        ["blend-subtitles"]     = "video",                
-        ["hwdec"] = "cuda",
-        ["video-sync"] = "display-resample",
+        ["blend-subtitles"]     = "video",
 
     },
     
   [o.svp] = {  
-        ["vo"] = "opengl",
         ["scale"]  = "ewa_lanczossharp",
         ["cscale"] = "ewa_lanczossoft",
         ["dscale"] = "mitchell",
         ["tscale"] = "triangle",
         ["scale-antiring"]  = "0.8",
-        ["cscale-antiring"] = "0.9",        
+        ["cscale-antiring"] = "0.9",
+        
         ["dither-depth"]        = "auto",
         ["target-prim"]         = "bt.709",
         ["correct-downscaling"] = "yes",
-        ["input-ipc-server"]  =   "/tmp/mpvpipe",        
-        ["hwdec"] = "no",
-        ["video-sync"] =  "display-resample",
+        ["input-ipc-server"]  =   "/tmp/mpvpipe",
     },
     
     [o.mq] = {
-        ["vo"] = "opengl",
         ["scale"]  = "spline36",
         ["cscale"] = "spline36",
         ["dscale"] = "mitchell",
@@ -119,136 +127,100 @@ local options = {
         ["scale-antiring"]  = "0.8",
         ["cscale-antiring"] = "0.9",
         ["scale-radius"]    = "3",
+
         ["dither-depth"]        = "auto",
         ["scaler-resizes-only"] = "yes",
         ["sigmoid-upscaling"]   = "yes",
-        ["blend-subtitles"]     = "yes",
-        ["interpolation"]     =   "no",
+
+        ["interpolation"]     =  function () return is_high_res(o) and "no" or "yes" end,
         ["interpolation-threshold"] = "0.0001",
         ["correct-downscaling"] = "yes",
         ["deband"]            = "yes",
-        ["opengl-waitvsync"]           = "yes",
-        ["target-prim"]         = "bt.709",
+        ["waitvsync"]           = "yes",
+        
+        ["gamma"]                = "0.9338",
+        ["target-prim"]         = "bt.2020",
+        ["target-trc"]          = "bt.1886",
         ["3dlut-size"]        = "256x256x256",
         ["blend-subtitles"]     = "yes",
         ["icc-contrast"]            = "2000",
-        ["icc-profile"]               = "/usr/share/color/icc/BT.709_Profiles/BT.709.icc",        
-        ["hwdec"] = "auto",
-        ["video-sync"] = "display-resample",
+        ["icc-profile"]               = "/usr/share/color/icc/BT.709_Profiles/BT.709.icc",
     },
 
     [o.lq] = {
-        ["vo"] = "opengl",
         ["scale"]  = "spline36",
         ["dscale"] = "mitchell",
         ["tscale"] = "triangle",
+
         ["dither-depth"]        = "auto",
         ["target-prim"]         = "bt.709",
         ["scaler-resizes-only"] = "yes",
         ["sigmoid-upscaling"]   = "yes",
         ["blend-subtitles"]     = "yes",
-        ["opengl-waitvsync"]           = "yes",
-        ["interpolation"]     = "no",
+        ["waitvsync"]           = "yes",
+
+        ["interpolation"]     = function () return is_high_res(o) and "no" or "yes" end,
         ["blend-subtitles"]     = "yes",
         ["icc-contrast"]            = "2000",
         ["icc-profile"]               = "/usr/share/color/icc/BT.709_Profiles/BT.709.icc",
-        ["hwdec"] = "no",
-        ["video-sync"] = "audio",
-        
     },
 }
 
 
--- Select the options level appropriate for this computer
-function determine_level(o, options)
-    -- Default level
-    local level = o.lq
+-- Specify general mpv options for different levels
 
-    -- Overwrite level from command line with --script-opts=ao-level=<level>
-    local overwrite = mp.get_opt("ao-level")
-    if overwrite then
-        if not options[overwrite] then
-            print("Forced level does not exist: " .. overwrite)
-            return level
-        end
-        return overwrite
-    end
+options = {
+    [o.hq] = {
+        ["options/vo"] = function () return vo_property_string(o.hq, vo, vo_opts) end,
+        ["options/hwdec"] = "auto",
+        ["options/video-sync"] = function () return is_high_res(o) and "audio" or "display-resample" end,
+        ["options/vd-lavc-threads"] = "16",
+        ["options/vf-add"] = "vdpaupp=sharpen=0.05:denoise=0.05:deint=no:deint-mode=temporal-spatial:hqscaling=1",
+     
+      [o.cuda] = {
+     ["options/vo"] = function () return vo_property_string(o.mq, vo, vo_opts) end,
+        ["options/video-sync"] = function () return is_high_res(o) and "audio" or "display-resample" end,
+        ["options/hwdec"] = "cuda",
+    },
+        
+    },
+  [o.svp] = {
+     ["options/vo"] = function () return vo_property_string(o.mq, vo, vo_opts) end,
+        ["options/video-sync"] = function () return is_high_res(o) and "audio" or "display-resample" end,
+        ["options/hwdec"] = "no",
+    },
+    [o.mq] = {
+        ["options/vo"] = function () return vo_property_string(o.mq, vo, vo_opts) end,
+        ["options/video-sync"] = function () return is_high_res(o) and "audio" or "display-resample" end,
+        ["options/hwdec"] = "auto",
+    },
 
-    -- Call an external bash function determining whether this is a desktop or laptop
-    local loc = exec({"/usr/bin/acpitool", "-b", "| grep AC adapter",})
-        if loc.status == "<not available>." then
-        level = o.lq
-        else
-        level = o.hq
- end
-
-    return level
-end
-
-
--- Determine if the currently used resolution is higher than o.highres_threshold
-function high_res_desktop(o)
-    sp_ret =  exec({"/usr/bin/inxi", "-xSGI", "| grep Resolution", "compare", o.highres_threshold})
-    return not sp_ret.error and sp_ret.status > 2
-end
-
-
-function high_res_video(o)
-    print("TODO: high_res_video(o)")
-    return false
-end
-
-
-function exec(process)
-    p_ret = utils.subprocess({args = process})
-    if p_ret.error and p_ret.error == "init" then
-        print("ERROR executable not found: " .. process[1])
-    end
-    return p_ret
-end
-
-
-function set_ASS(b)
-    return mp.get_property_osd("osd-ass-cc/" .. (b and "0" or "1"))
-end
-
-
-function red_border(s)
-    return set_ASS(true) .. "{\\bord1}{\\3c&H3300FF&}{\\3a&H20&}" .. s .. "{\\r}" .. set_ASS(false)
-end
-
-
-function print_status(name, value, o)
-    if not value or not o.level then
-        return
-    end
-
-    if o.err_occ then
-        print("Error setting level: " .. o.level)
-        mp.osd_message(red_border("Error setting level: ") .. o.level, o.duration * o.duration_err_mult)
-    else
-        print("Active level: " .. o.level)
-        mp.osd_message(o.level
-             .. (high_res_desktop(o) and "\n↳ desktop: high res" or "")
-             .. (high_res_video(o) and "\n↳ video: high res" or ""), o.duration)
-    end
-    mp.unobserve_property(print_status)
-end
+    [o.lq] = {
+        ["options/vo"] = function () return vo_property_string(o.lq, vo, vo_opts) end,
+        ["options/video-sync"] = "audio",
+        ["options/hwdec"] = "auto",
+    },
+}
 
 
 -- Print status information to VO window and terminal
+
 mp.observe_property("vo-configured", "bool",
                     function (name, value) print_status(name, value, o) end)
 
 
--- Determined level and apply the appropriate options
+-- Determined level and set the appropriate options
+
 function main()
-    o.level = determine_level(o, options)
+    o.force_low_res = mp.get_opt("ao-flr")
+    o.level = determine_level(o, vo, vo_opts, options)
     o.err_occ = false
     for k, v in pairs(options[o.level]) do
         if type(v) == "function" then
             v = v()
         end
+        print(k)
+        print(v)
         success, err = mp.set_property(k, v)
         o.err_occ = o.err_occ or not (o.err_occ or success)
         if success and o.verbose then
